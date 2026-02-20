002b0de5c3c6681bb0a4e66cacaaa3f3621d7482
Kévin <kevin.painchaud@gmail.com>
2026-02-20 08:17:27 +0100

    fix(ButtonSkeleton): update width of skeleton button to 36

--- src/components/ButtonSkeleton/ButtonSkeleton.tsx
+++ src/components/ButtonSkeleton/ButtonSkeleton.tsx
@@ -5,7 +5,7 @@ import type { ButtonSkeletonProps } from "./types";
 export const ButtonSkeleton = ({ size = "base" }: ButtonSkeletonProps) => {
   return (
     <div
-      className={classNames("skeleton w-40 rounded-full", {
+      className={classNames("skeleton w-36 rounded-full", {
         "h-8": size === "sm",
         "h-11": size === "base",
         "h-14": size === "lg",
