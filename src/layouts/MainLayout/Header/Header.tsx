35800e3176f06cfb214c033f4e776e1c0fafbbff
Kévin <kevin.painchaud@gmail.com>
2026-03-15 09:16:12 +0100

    feat(Header): add product hunt embed

--- src/layouts/MainLayout/Header/Header.tsx
+++ src/layouts/MainLayout/Header/Header.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { PiListBold } from "react-icons/pi";
 import { Link } from "react-router";
@@ -7,10 +8,14 @@ import { Button } from "../../../components/Button/Button";
 import { Logo } from "../../../components/Logo/Logo";
 import { ThemeSwitcherButton } from "../../../components/ThemeSwitcherButton/ThemeSwitcherButton";
 import { UserDropdown } from "../../../components/UserDropdown/UserDropdown";
+import { APP_GLOBAL_VARIABLES } from "../../../constants/appGlobalVariables";
+import { UserPreferencesContext } from "../../../context/UserPreferencesContext";
 import { useDrawer } from "../../../hooks/useDrawer";
 import type { HeaderProps } from "./types";
 
 export const Header = ({ className }: HeaderProps) => {
+  const { darkThemeEnabled } = useContext(UserPreferencesContext);
+
   const { setMainMenuDrawerOpen } = useDrawer();
   const { t } = useTranslation();
 
@@ -20,6 +25,19 @@ export const Header = ({ className }: HeaderProps) => {
         <Link className="default-style-none" to="/">
           <Logo showBeta />
         </Link>
+        <a
+          className="hidden lg:flex"
+          href="https://www.producthunt.com/products/campee-planning-poker-online?embed=true&utm_source=badge-top-post-badge&utm_medium=badge&utm_campaign=badge-campee"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            alt={`${APP_GLOBAL_VARIABLES.app_name} - ${t("common.app_baseline")} | Product Hunt`}
+            height="40"
+            src={`https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=1093728&theme=${darkThemeEnabled ? "dark" : "light"}&period=daily&t=1773561333713`}
+            width="186"
+          />
+        </a>
         <div className="flex grow justify-end">
           <Button
             className="lg:hidden"
