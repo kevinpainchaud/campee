8ea2f13f32e3b3d7e832ae3df780983216404b58
Kévin <kevin.painchaud@gmail.com>
2026-03-19 20:46:45 +0100

    fix(ParticipantCard): prevent card selection if user is not a participant

--- src/components/VotingRoomTable/ParticipantCard/ParticipantCard.tsx
+++ src/components/VotingRoomTable/ParticipantCard/ParticipantCard.tsx
@@ -68,7 +68,7 @@ export const ParticipantCard = ({
                     onClick={() =>
                       setActionShown((actionShown) => !actionShown)
                     }
-                    tagElement="button"
+                    tagElement={userParticipant ? "button" : "div"}
                   />
                 ) : (
                   <PlaceholderCard
@@ -76,7 +76,7 @@ export const ParticipantCard = ({
                     onClick={() =>
                       setActionShown((actionShown) => !actionShown)
                     }
-                    tagElement="button"
+                    tagElement={userParticipant ? "button" : "div"}
                   />
                 )
               }
